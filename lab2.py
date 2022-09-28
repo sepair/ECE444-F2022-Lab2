ec045ef8b7335111c7685cd8508cd260ccec4a25
stahmasebi <sepehr.tahmasebi@student.unimelb.edu.au>
2022-09-28 02:20:24 -0400

    exmaples from chapter 4 complete

--- lab2.py
+++ lab2.py
@@ -1,15 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, validators
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[validators.DataRequired()]) 
+    name2 = StringField('What is your UofT email?', validators=[validators.DataRequired(), validators.email()]) 
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
 boostrap= Bootstrap(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = 'this should be hard to guess'
 
 @app.route('/') 
 def index():
     return render_template('index.html')
+
+@app.route('/form', methods=['GET', 'POST']) 
+def form():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!') 
+        session['name'] = form.name.data
+        session['name2'] = form.name2.data
+        form.name.data = form.name2.data = ''
+        return redirect(url_for('form'))
+    return render_template('form.html', form = form, name = session.get('name'), name2 = session.get('name2'))
 
 @app.route('/user/<name>') 
 def user(name):
